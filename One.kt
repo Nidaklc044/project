4fa60c096dff40cd13d5e4e387bbc88b35be9a50
Nidaklc044 <116462585+Nidaklc044@users.noreply.github.com>
2023-03-21 19:33:42 +0300

    Add files via upload
    Kotlin

---
+++ One.kt
@@ -0,0 +1,161 @@
+package com.nidakilic.project
+
+fun main(){
+
+var sehir ="Malatya"
+    println(sehir)
+
+    var ulke="Turkiye"
+        println(ulke)
+
+    var telefon =5374852357
+        println(telefon)
+
+    var postaKodu=441
+        println(postaKodu)
+
+    var email="nida@gmail.com"
+        println(email)
+
+    var meslek ="mühendis"
+        println(meslek)
+
+    var stokMiktarı =558455
+        println(stokMiktarı)
+
+    var musteriAdı="Elif"
+        println(musteriAdı)
+
+    var bakiye =275
+        println(bakiye)
+
+    var dogumGunu="20.05.1985"
+        println(dogumGunu)
+
+    var maas=10000
+        println(maas)
+
+    var medeniDurum ="Bekar"
+        println(medeniDurum)
+
+    var urunYorum ="begenildi"
+        println(urunYorum)
+
+    var odemeTarihi ="17.07.2023"
+        println(odemeTarihi)
+
+    var odeme =2500
+        println(odeme)
+
+    var siparisAdedi=25
+        println(siparisAdedi)
+
+    var arabaModeli="Ford"
+        println(arabaModeli)
+
+    var kitapAdı="Suç ve Ceza"
+        println(kitapAdı)
+
+    var yayınlanmaTarihi ="10.04.2015"
+        println(yayınlanmaTarihi)
+
+    var indirimMiktarı="%10"
+        println()
+
+    var odaSayısı=4
+        println(odaSayısı)
+
+    var enlem= "36-42"
+        println(enlem)
+
+    var boylam="26-45"
+        println(boylam)
+
+    var urunAdı ="kitap"
+        println(urunAdı)
+
+    var yemekFiyati=125
+        println(yemekFiyati)
+
+    var marka="Ulker"
+        println(marka)
+
+    var muzikAdı="Dombra"
+        println(muzikAdı)
+
+    var videoSuresi=4.5
+        println(videoSuresi)
+
+    var urunPuani=4.2
+        println(urunPuani)
+
+    var resimAdi="arsresim"
+        println(resimAdi)
+
+    var dosyaFormati="jpg"
+        println(dosyaFormati)
+    var renk="Mavi"
+        println(renk)
+
+    var renkKodu=410254
+        println(renkKodu)
+
+    var telefonModeli="Apple"
+        println(telefonModeli)
+
+    var ekranBoyutu="7 inç"
+        println(ekranBoyutu)
+
+    var ağirlik=0.200
+        println(ağirlik)
+
+    var ulusalGun="23 Nisan"
+        println(ulusalGun)
+
+    var tatilGunu="Pazar"
+        println(tatilGunu)
+
+    var rezervasyonTarihi="2 Nisan 2023"
+        println(rezervasyonTarihi)
+
+    var sokakAdı="Universite Sokagı"
+        println(sokakAdı)
+
+    var otobusHattı="bilinmiyor"
+        println(otobusHattı)
+
+    var kalanDakika=28
+        println(kalanDakika)
+
+    var takipKodu="A145455455"
+        println(takipKodu)
+
+    var kuponSuresi="3 gün"
+        println(kuponSuresi)
+
+    var kuponKodu="AZ457KD"
+        println(kuponKodu)
+
+    var faturaAdresi="istanbul-Beşiktaş "
+        println(faturaAdresi)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
